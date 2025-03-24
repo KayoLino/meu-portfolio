f3ed4483759993560e947a9af2fb7e3d95734120
KayoLino <kayofelipelino2002@gmail.com>
2025-03-24 11:42:01 -0300

    Atualizando responsividade

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './styles/base/global.css';
+import './styles/base/app.css';
 import './styles/base/animation.css';
 import Desktop from './pages/Desktop';
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles/base/global.css';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
